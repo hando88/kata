dd8f3efe3ac64c2616b33736c9df357583281321
hando <97342288+hando88@users.noreply.github.com>
2022-09-09 18:13:07 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,37 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(new User("Kata", "Academy", "info@kata.academy", new Car("Tesla", 10)));
+
+        userService.add(new User("Aleksey", "Kuznetsov", "alexey@gmail.com", new Car("BMW", 550)));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println(user.getCar());
+            System.out.println();
+        }
+
+        System.out.println(userService.getCarsUser("BMW", 550));
+
+        context.close();
+    }
+}
---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,16 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+
+    List<User> listUsers();
+
+    @Transactional
+    User getCarsUser(String model, int series);
+}
