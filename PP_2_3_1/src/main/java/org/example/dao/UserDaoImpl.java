65937181b614efbc95b4df1c19f74fa4d23961cb
hando <97342288+hando88@users.noreply.github.com>
2022-09-29 16:58:58 +0300

    Update UserDaoImpl.java

--- PP_2_3_1/src/main/java/org/example/dao/UserDaoImpl.java
+++ PP_2_3_1/src/main/java/org/example/dao/UserDaoImpl.java
@@ -14,32 +14,27 @@ public class UserDaoImpl implements UserDao {
     private EntityManager entity;
 
     @Override
-    @Transactional
     public List<User> index() {
         return entity.createQuery("select c from User c", User.class).getResultList();
     }
 
     @Override
-    @Transactional
     public void save(User user) {
         entity.persist(user);
     }
 
     @Override
-    @Transactional
     public void delete(long id) {
         User user = entity.find(User.class, id);
         entity.remove(user);
     }
 
     @Override
-    @Transactional
     public User getUserById(long id) {
         return entity.find(User.class, id);
     }
 
     @Override
-    @Transactional
     public void update(User user) {
         entity.merge(user);
     }
