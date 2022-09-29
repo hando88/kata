42e614dde18e09363e6058f5f8fe83d36a6cc2ee
hando <97342288+hando88@users.noreply.github.com>
2022-09-29 17:02:04 +0300

    Update UserServiceImpl.java

--- PP_2_3_1/src/main/java/org/example/service/UserServiceImpl.java
+++ PP_2_3_1/src/main/java/org/example/service/UserServiceImpl.java
@@ -15,26 +15,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public List<User> index() {
         return userDao.index();
     }
 
     @Override
+    @Transactional
     public void save(User user) {
         userDao.save(user);
     }
 
     @Override
+    @Transactional
     public void delete(long id) {
         userDao.delete(id);
     }
 
     @Override
+    @Transactional
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
 
     @Override
+    @Transactional
     public void update(User user) {
         userDao.update(user);
     }
